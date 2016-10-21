4c00d32891a278d62bc3b04a8999d356b8942060
Dakota Degracia <dakota.degracia@gmail.com>
2016-10-21 16:03:25 -0700

    started the projected
    i add team name and score and made it so i can add 1 point to the team

--- app/src/main/java/com/example/student/court_counter/MainActivity.java
+++ app/src/main/java/com/example/student/court_counter/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.student.court_counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int scoreTeamAwesome = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayForTeamAwesome(0);
+    }
+
+    private void displayForTeamAwesome(int score){
+        TextView scoreTeamAwesome = (TextView) findViewById(R.id.txtVw_scoreTeamAwesome);
+        if(scoreTeamAwesome!=null)
+            scoreTeamAwesome.setText(String.valueOf(score));
+    }
+
+    public void addOneForTeamAwesome(View view) {
+        int step = 1;
+        scoreTeamAwesome += step;
+        displayForTeamAwesome(scoreTeamAwesome);
     }
 }
